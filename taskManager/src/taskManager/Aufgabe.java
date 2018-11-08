817d20a18cf2e636069ed62414839c3807314da2
Alex <Alex@Alexander-PC>
2018-11-08 14:26:09 +0100

    Hallo Paul isch kackmann

--- taskManager/src/taskManager/Aufgabe.java
+++ taskManager/src/taskManager/Aufgabe.java
@@ -1,11 +1,18 @@
 package taskManager;
 
+import java.security.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Date;
+import java.sql.Timestamp;
 
 public class Aufgabe {
 
 	private String aufgabentext;
 	private GregorianCalendar datumErstellung;
+	
 	
 
 	public Aufgabe(String aufgabentext, GregorianCalendar datumErstellung) {
@@ -34,4 +41,26 @@ public class Aufgabe {
 	public String toString() {
 		return "Aufgabe --> " + this.getAufgabentext() + "\n" + "Erstellungsdatum --> " + this.getDatumErstellung();
 	}
+	
+	public static Calendar stringToCalendar(String stringDate, String datePattern) {
+	    if (stringDate == null) {
+	      return null;
+	    }
+	    Calendar calendar = new GregorianCalendar();
+	    try {
+	      Timestamp newDate = Timestamp.valueOf(stringDate);
+	      calendar.setTime(newDate);
+	    }
+	    catch (Exception e) {
+	      SimpleDateFormat simpleDateFormat = new SimpleDateFormat(datePattern);
+	      try {
+	        calendar.setTime(simpleDateFormat.parse(stringDate));
+	      }
+	      catch (ParseException pe) {
+	        calendar = null;
+	      }
+	    }
+	    return calendar;
+	  }
+	 
 }
