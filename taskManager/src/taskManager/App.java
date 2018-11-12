1a247c1238c0ead7d0b27f136719e632a634f29c
paphill <44397991+paphill@users.noreply.github.com>
2018-11-12 14:50:13 +0100

    Update App.java

--- taskManager/src/taskManager/App.java
+++ taskManager/src/taskManager/App.java
@@ -1,12 +1,9 @@
 package taskManager;
 
-import java.text.SimpleDateFormat;
-
 public class App {
 	
 	public static void main(String[] args) {
-//		Aufgabe a = new Aufgabe("APR-Hü,", 21, 11, 2018);
-//		SimpleDateFormat datumformat = new SimpleDateFormat("dd.mm.yyyy");
-//		System.out.println(datumformat.format(a.getDatumErstellung()));
+		Kommandozeilenmenue kz = new Kommandozeilenmenue();
+		kz.start();
 	}
 }
